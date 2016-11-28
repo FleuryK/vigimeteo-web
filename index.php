d432f1821df3335f8c8a2784204ba446fe618d77
Kévin <kevinfleury.todae@gmail.com>
2016-11-28 18:30:21 +0100

    Mini-update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$file_xml = simplexml_load_file("../carte_vigilance_meteo.xml");
+$file_xml = simplexml_load_file("../carte_vigilance_meteo.xml");	// À adapter selon ou est situé votre fichier XML
 
 $dept_cotes = array('cote_06', 'cote_11', 'cote_13', 'cote_14', 'cote_17', 'cote_22', 'cote_29', 'cote_2A', 'cote_2B', 'cote_30', 'cote_33', 'cote_34',
 'cote_35', 'cote_40', 'cote_44', 'cote_50', 'cote_56', 'cote_59', 'cote_62', 'cote_64', 'cote_66', 'cote_76', 'cote_80', 'cote_83', 'cote_85');
